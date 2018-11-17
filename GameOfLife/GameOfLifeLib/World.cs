e474c8eaa1c5b428d50d786768cd66e15c456ff2
Kazuki Kachi <10533296+Kazuki-Kachi@users.noreply.github.com>
2018-11-18 04:20:14 +0900

    とにかく通す

---
+++ GameOfLife/GameOfLifeLib/World.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GameOfLifeLib
+{
+    public class World
+    {
+        private int x;
+        private int y;
+
+        public World(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public IEnumerable<bool> State => Enumerable.Range(0, y).SelectMany(_ => Enumerable.Range(0, x), (y, x) => false);
+    }
+}
