b98da3db9fb383a8ac4fc2544bbfbd2907cee8a4
Kazuki Kachi <10533296+Kazuki-Kachi@users.noreply.github.com>
2018-11-18 03:49:35 +0900

    通らないテストを書く（コンパイルエラー）

--- GameOfLife/GameOfLifeLibTest/WorldTest.cs
+++ GameOfLife/GameOfLifeLibTest/WorldTest.cs
@@ -1,9 +1,16 @@
 using System;
 using Xunit;
+using GameOfLifeLib;
 
 namespace GameOfLifeLibTest
 {
     public class WorldTest
     {
+        [Fact]
+        public void ”CˆÓ‚ÌL‚³‚Ì¢ŠE‚ğì‚é–‚ª‚Å‚«‚é()
+        {
+            var world = new World(10, 10);
+            Assert.Equal(10 * 10, world.State.Count());
+        }
     }
 }
