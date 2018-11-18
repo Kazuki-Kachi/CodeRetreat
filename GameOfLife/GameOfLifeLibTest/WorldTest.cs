4860ac61127b119d0496deda06ace6659bacfcf8
Kazuki Kachi <10533296+Kazuki-Kachi@users.noreply.github.com>
2018-11-19 04:35:00 +0900

    テストコードの修正(結果は変わらない)と、パターン(広さのない世界を生成することができない)を追加

--- GameOfLife/GameOfLifeLibTest/WorldTest.cs
+++ GameOfLife/GameOfLifeLibTest/WorldTest.cs
@@ -9,14 +9,20 @@ namespace GameOfLifeLibTest
     public class WorldTest
     {
 
-        public static IEnumerable<object[]> hoge => Enumerable.Range(1, sbyte.MaxValue).Zip(Enumerable.Range(1, sbyte.MaxValue), (x, y) => new object[] { x, y });
-
+        public static IEnumerable<object[]> ¢ŠE‚ÌL‚³ { get; } = Enumerable.Range(1, sbyte.MaxValue).SelectMany(_ => Enumerable.Range(1, sbyte.MaxValue), (x, y) => new object[] { x, y });
         [Theory]
-        [MemberData(nameof(hoge))]
-        public void ”CˆÓ‚ÌL‚³‚Ì¢ŠE‚ðì‚éŽ–‚ª‚Å‚«‚é(int x,int y)
+        [MemberData(nameof(¢ŠE‚ÌL‚³))]
+        [InlineData(data: new object[] { 1000, 1000 })]
+        public void ”CˆÓ‚ÌL‚³‚Ì¢ŠE‚ðì‚éŽ–‚ª‚Å‚«‚é(int x, int y)
         {
             var world = new World(x, y);
-            Assert.DoesNotContain(world.State, states => states.Any(state => state));
+            Assert.DoesNotContain(world.State.AsParallel(), states => states.Any(state => state));
         }
+
+        public static IEnumerable<object[]> ¬—§o—ˆ‚È‚¢¢ŠE‚ÌL‚³ { get; } = Enumerable.Range(-25, 50).SelectMany(x => x >= 0 ? Enumerable.Range(-25, 26) : Enumerable.Range(1, 24), (x, y) => new object[] { x, y });
+        [Theory]
+        [MemberData(nameof(¬—§o—ˆ‚È‚¢¢ŠE‚ÌL‚³))]
+        public void ¢ŠE‚Ìc‚Æ‰¡‚ÍÅ’á1ˆÈã‚ ‚é(int x, int y)
+            => Assert.Throws<ArgumentOutOfRangeException>(() => new World(x, y));
     }
 }
